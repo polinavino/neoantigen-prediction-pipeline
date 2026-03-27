3d965e3b9140ca84fce58d4839335c4d1904ff80
polinavino <polina.vino@gmail.com>
2026-03-27 15:08:38 -0400

    Update apply_patches.sh

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -5,5 +5,6 @@ sed -i "" "s/asparagine_proline_bond_count/aspartate_proline_bond_count/g" $PVAC
 sed -i "" "s|http://tools-cluster-interface.iedb.org/tools_api/mhci/|https://tools-cluster-interface.iedb.org/tools_api/mhci/|g" $PVAC_LIB/prediction_class.py
 sed -i "" "s|http://tools-cluster-interface.iedb.org/tools_api/mhcii/|https://tools-cluster-interface.iedb.org/tools_api/mhcii/|g" $PVAC_LIB/prediction_class.py
 sed -i "" "s/requests.post(self.url, data=data)/requests.post(self.url, data=data, timeout=300)/g" $PVAC_LIB/prediction_class.py
+sed -i "" 's/arguments = \["python", script/arguments = ["python3", script/' $PVAC_LIB/prediction_class.py
 
 echo "Patches applied successfully"
